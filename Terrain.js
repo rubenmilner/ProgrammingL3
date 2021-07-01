cfd495507c569444096826f5a7fb4ad96e6e60b0
rubenmilner <51342778+rubenmilner@users.noreply.github.com>
2021-07-01 13:31:38 +1200

    Add files via upload
    terrain spawns at random height

---
+++ Terrain.js
@@ -0,0 +1,15 @@
+class Terrain {
+    constructor(x, y, w, h, c) {
+      this.x = x;
+      this.y = y;
+      this.w = w;
+      this.h = h;
+      this.c = c;
+    }
+
+    drawRect() {
+      canvasContext.fillStyle = this.c;
+      canvasContext.fillRect(this.x, this.y, this.w, this.h);
+    }
+
+}
